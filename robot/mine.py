1078f82ba0db8d0eb47e5dc7699ef58af6593b83
chloea <chloeayoub@yahoo.co.uk>
2014-08-19 15:01:11 +0100

    Running robot/mine.py

--- robot/mine.py
+++ robot/mine.py
@@ -6,4 +6,8 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
+def go():
+  moved=0
+  while touch () == "fruit":
+    move ()
+    moved=moved +1
