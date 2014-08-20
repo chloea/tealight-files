a4e7c8a897494e1d7dd68b8c60b0abd05080ed10
chloea <chloea@users.noreply.github.com>
2014-08-20 10:55:53 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -7,11 +7,9 @@ width = 10
 height = 8
 
 for i in range(0,width):
-  print i
   for j in range(0,height):
-    print "%d, %d" % (i, j)
     if i % 4 == 0:
-      image(x + i * 80, y + j * 50, "misc/Sword.png")
+      image(x + i * 30, y + j * 30, "misc/Sword.png")
     else:
-      image(x + i * 60, y + j * 60, "misc/PirateFlag.png")
+      image(x + i * 30, y + j * 30, "misc/PirateFlag.png")
      
