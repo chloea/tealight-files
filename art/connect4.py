d01836dd4cf951be86ecb3b5e90c85c0b0cce3a4
chloea <chloea@users.noreply.github.com>
2014-08-21 14:28:18 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -26,3 +26,20 @@ def handle_mousedown(x,y,button):
 
 
 
+#board
+color("blue")
+box(20,80,850,840)
+
+x = 95
+y = 150
+
+width = 20
+height = 8
+
+for i in range(0,8):
+  for j in range(0,8):
+    if (i + j)%4 ==0:
+      color("white")
+      spot(x + i * 100, y + j * 100, 40)
+    else:
+      spot(x + i * 100, y + j * 100, 40)
