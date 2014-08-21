e82d2606e48c4fd28e347dca00714e3c7b2ed2a9
chloea <chloea@users.noreply.github.com>
2014-08-21 10:44:35 +0100

    Closing art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,12 +1,5 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 
-def handle_mousedown(x,y):
-  color("red")
-  spot(x,y,10)
-  
-def handle_mousemove(x,y,right):
-  if button == "right":
-    color("red")
-    circle(x,y,10)
+
  
