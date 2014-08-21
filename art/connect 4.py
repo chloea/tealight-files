c5a58def48a4f682eb0c31bec7d8cc2dad3bfbfa
chloea <chloea@users.noreply.github.com>
2014-08-21 14:27:36 +0100

    Closing art/connect 4.py

--- art/connect 4.py
+++ art/connect 4.py
@@ -1,4 +1,19 @@
-from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-for x in range (0,50) 
-for y in range (0,50)
+
+#board
+color("blue")
+box(20,80,850,840)
+
+x = 95
+y = 150
+
+width = 20
+height = 8
+
+for i in range(0,8):
+  for j in range(0,8):
+    if (i + j)%4 ==0:
+      color("white")
+      spot(x + i * 100, y + j * 100, 40)
+    else:
+      spot(x + i * 100, y + j * 100, 40)
