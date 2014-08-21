b63e0a2bc5cbf5f007c3c3fa063b6912cc79c2f1
chloea <chloea@users.noreply.github.com>
2014-08-21 11:04:52 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -3,14 +3,16 @@ from tealight.art import (color, line,circle, spot, box, image, text, background
 
 
  
-def handle_mousedown(x,y,"left"):
+def handle_mousedown(x,y,button):
+  if button == "left":
   color("red")
   spot(x,y,50)
+   elif button == "right":
+   color("yellow")
+   spot (x,y,50))
   
 
   
-def handle_mousedown(x,y,"right"):
-  color("yellow")
-  spot(x,y,50)
+
   
 
