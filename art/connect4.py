02814af3254c798df85fea3b3d8159c4aff6b2c4
chloea <chloea@users.noreply.github.com>
2014-08-21 14:14:22 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,12 +1,16 @@
 from tealight.art import (color, line,circle, spot, box, image, text, background)
 turn = 2
 
-
-
-
+def add_counter(col):
+  print "Adding counter to column " + col
+  
 
 def handle_mousedown(x,y,button):
   global turn 
+  
+  add_counter(x)
+  
+  
   if button == "left" and turn ==1:
     color("red")
     spot(x,y,40)
