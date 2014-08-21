fe41ad05d090ed6ea462e5ed55bd574143df178a
chloea <chloea@users.noreply.github.com>
2014-08-21 12:20:26 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,7 +1,10 @@
 from tealight.art import (color, line,circle, spot, box, image, text, background)
 
-n=0
-
+def n:
+  if n/2 != int:
+    turn=1
+  elif n/2 = int:
+    turn=2
  
 def handle_mousedown(x,y,button):
   if button == "left":
@@ -10,20 +13,24 @@ def handle_mousedown(x,y,button):
   elif button == "right":
     color("yellow")
     spot (x,y,40)
+    
+    
+#def do_stuff(turn):
+ # if n =
   
 
 
-while n<64:
- def handle_mousemove(x,y,button):
-  if button == 'right':
-    turn = 2
-    color("red")
-    spot(x,y,40)
-  elif button == 'left':
-    color("yellow")
-    spot(x,y,40)
-    turn = 1
-  n=n+1
+#while n<64:
+ #def handle_mousemove(x,y,button):
+  #if button == 'right':
+   # turn = 2
+    #color("red")
+    #spot(x,y,40)
+  #elif button == 'left':
+    #color("yellow")
+    #spot(x,y,40)
+    #turn = 1
+  #n=n+1
   
 #if turn = 1:
 
