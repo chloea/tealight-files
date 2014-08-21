0d096eba05c911aea3a092e2bba6a3d09c2ea750
chloea <chloea@users.noreply.github.com>
2014-08-21 12:06:19 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -12,7 +12,15 @@ def handle_mousedown(x,y,button):
     spot (x,y,40)
   
 
-  
 
+  while n<64:
+  def handle_mousemove(x,y,button):
+  if button == 'left':
+    turn = 2
+  elif button == 'left':
+    turn = 1
+  n=n+1
   
+if turn = 1:
+  color(
 
