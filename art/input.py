73751a169e19dfe70bdea7a00a10a733c22d3270
chloea <chloea@users.noreply.github.com>
2014-08-22 10:56:58 +0100

    Running art/input.py

--- art/input.py
+++ art/input.py
@@ -1,7 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 def handle_mousedown(x,y):
-  color("red")
+  color("yellow")
   spot(x,y,50)
   
 def handle_mousemove(x,y,button):
