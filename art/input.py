93c4996209be053e7c73ea05deef432730ccfe94
chloea <chloea@users.noreply.github.com>
2014-08-22 10:54:55 +0100

    Running art/input.py

--- art/input.py
+++ art/input.py
@@ -2,9 +2,9 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 def handle_mousedown(x,y):
   color("red")
-  spot(x,y,10)
+  spot(x,y,50)
   
 def handle_mousemove(x,y,button):
   if button == "left":
     color("red")
-    circle(x,y,10)
+    circle(x,y,50)
