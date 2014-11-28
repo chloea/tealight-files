329f5dc356081d7f5fd6f2a1adb1f70c3e19237b
chloea <chloea@users.noreply.github.com>
2014-11-28 14:55:12 +0000

    Running logo/fractal.py

--- logo/fractal.py
+++ logo/fractal.py
@@ -7,13 +7,13 @@ def segment(scale, detail):
   if detail == 1:
     move(scale)
   else:
-    segment(scale / 3.0, detail - 2)
+    segment(scale / 1.0, detail - 1)
     turn(-60)
-    segment(scale / 3.0, detail - 1)
+    segment(scale / 1.0, detail - 1)
     turn(120)
-    segment(scale / 3.0, detail - 1)
+    segment(scale / 1.0, detail - 1)
     turn(-60)
-    segment(scale / 3.0, detail - 1)
+    segment(scale / 1.0, detail - 1)
     
 
 turn(360)
